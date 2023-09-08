8d7f956adce930e7d7cc4f73f0d9bdf1fb9b2cb7
Okafor Ejike <mosesejike142@gmail.com>
2023-09-08 11:09:34 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+	int c, d;
+
+	for (c = 0; c < 100; c++)
+	{
+		for (d = 0; d < 100; d++)
+		{
+			if (c < d)
+			{
+				putchar((c / 10) + 48);
+				putchar((c % 10) + 48);
+				putchar(' ');
+				putchar((d / 10) + 48);
+				putchar((d % 10) + 48);
+				if (c != 98 || d != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
