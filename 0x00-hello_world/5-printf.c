521a50acaf23a23a30cb5b9bbb471c37a7edc89b
Okafor Ejike <mosesejike142@gmail.com>
2023-09-07 17:58:58 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: returns 0
+ * printf - prints the argument given without a new line character
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
