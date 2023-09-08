266984b938168d6a5f3fabb15f87161aa4247bad
Okafor Ejike <mosesejike142@gmail.com>
2023-09-07 17:46:41 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - execution starts from here
+ * puts - prints the argument given with a new ine character by default
+ * Return: the function returns an int 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	/**
+	 * return - returns an int "0"
+	 */
+	return (0);
+}
