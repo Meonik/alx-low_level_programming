fed5e7bb3211352ec4ca3c7400c5ffcc2a6aecf8
Okafor Ejike <mosesejike142@gmail.com>
2023-09-12 09:35:02 -0700

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet
+ * Return: void
+ */
+
+void print_alphabet(void)
+{
+/* print_alphabet function has a prototye in main.h */
+/*and has been called in 1-main.c*/
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+		_putchar('\n');
+	}
+}
