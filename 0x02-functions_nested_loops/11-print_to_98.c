a3f5552269e6bb69b96fe0da142f7f784fe800e5
Okafor Ejike <mosesejike142@gmail.com>
2023-09-12 11:26:58 -0700

    a + b

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Adds the two values given
+ * @n1: first number
+ * @n2: second number
+ * Return: the result as an int type
+ */
+
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
